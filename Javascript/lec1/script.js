0206eb3603ac24ac0add14d608bb40fad2884613
Gajanan Ardalkar <gajananardalkar999@gmail.com>
2023-12-27 22:25:12 +0530

    javascript

---
+++ Javascript/lec1/script.js
@@ -0,0 +1,21 @@
+// alert("this is Rayan JS");
+
+// let
+// ,var,
+
+
+
+function addition( num1,  num2){
+    var result =  num1 + num2;
+    console.log(result);
+}
+
+function abcd(name){
+    console.log("this is before changing", name);
+    name="Gajanan"
+    let a=20,b=30
+    console.log(name);
+}
+addition(1,11);
+abcd("Bhavesh");
+
