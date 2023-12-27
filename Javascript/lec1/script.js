56e433f5c82e8f08c3c59f99da028213545945d1
Gajanan Ardalkar <gajananardalkar999@gmail.com>
2023-12-27 22:32:27 +0530

    updated JS

--- Javascript/lec1/script.js
+++ Javascript/lec1/script.js
@@ -1,21 +1,22 @@
 // alert("this is Rayan JS");
 
-// let
+// let 
 // ,var,
 
 
 
 function addition( num1,  num2){
-    var result =  num1 + num2;
+    let result =  num1 + num2;
     console.log(result);
 }
 
-function abcd(name){
-    console.log("this is before changing", name);
-    name="Gajanan"
-    let a=20,b=30
+function abcd(){
+let name="Gajanan"
     console.log(name);
 }
-addition(1,11);
-abcd("Bhavesh");
 
+// function call
+addition(1,11);
+abcd();
+
+// question: write a code for add, sub, div,mul all should be function, in two function pass values from function call, and in other functions declare value inside it, the result of all function should not able to change
